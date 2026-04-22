d93a9ff8c7fbb390f05416d6de2940dde9ef84c6
burak33bb <142359118+burak33bb@users.noreply.github.com>
2026-04-22 21:40:19 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:40:12.957Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-22T18:40:17.328Z
+Commit: Resolve timezone issues in date handling
