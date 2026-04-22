d7efde0447b3223f02dfe05225965d2140b1cb2e
burak33bb <142359118+burak33bb@users.noreply.github.com>
2026-04-22 21:40:10 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:40:02.591Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-22T18:40:07.949Z
+Commit: Fix CSS styling bug on mobile devices
