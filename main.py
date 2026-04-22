3bdf732be4741a01e9f9f5ccf2a34cce0638ce44
burak33bb <142359118+burak33bb@users.noreply.github.com>
2026-04-22 21:40:15 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:37:02.391Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-22T18:40:12.957Z
+Commit: Patch API endpoint error handling
