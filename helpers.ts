eef38533ad44f85527237b11b5f67a1e53b37d85
burak33bb <142359118+burak33bb@users.noreply.github.com>
2026-04-22 21:36:25 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:36:14.921Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-22T18:36:22.802Z
+Commit: Correct typo in configuration file
