e84f78f6c7623f3d4a2716f01d40f63c543b3b92
burak33bb <142359118+burak33bb@users.noreply.github.com>
2026-04-22 21:40:18 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:40:09.268Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-22T18:40:15.929Z
+Commit: Fix broken links in documentation
