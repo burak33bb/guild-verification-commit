d33d5e96db76f26f411e68bb388325438d1de572
burak33bb <142359118+burak33bb@users.noreply.github.com>
2026-04-22 21:36:47 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:36:30.449Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-04-22T18:36:45.217Z
+Commit: Resolve null pointer exception in user service
