bdf8009a40ab74077ebda779f23aa6359782c629
burak33bb <142359118+burak33bb@users.noreply.github.com>
2026-04-22 21:40:38 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:40:07.949Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-22T18:40:36.523Z
+Commit: Patch API endpoint error handling
