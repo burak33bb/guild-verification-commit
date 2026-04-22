5afd68f41206516be9dbf3e86ee60e156b9f6873
burak33bb <142359118+burak33bb@users.noreply.github.com>
2026-04-22 21:40:37 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:40:17.328Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-22T18:40:35.319Z
+Commit: Resolve null pointer exception in user service
